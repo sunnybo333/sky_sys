cafda9b5ddd69135c479dd675b8113e7a54da5d0
jason <sunnybo333@163.com>
2017-04-23 22:13:27 +0800

    更新

---
+++ git脚本/git-pull.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd /Applications/XAMPP/xamppfiles/htdocs/sky_sys
+git pull
+read -p
---
+++ git脚本/git-push.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd /Applications/XAMPP/xamppfiles/htdocs/sky_sys
+git add .
+git commit -m "更新"
+git push
+read -p
