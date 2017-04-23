72d89b9d1477a64f95d7f8d1d6b0cb923892e483
jason <sunnybo333@163.com>
2017-04-23 21:27:09 +0800

    2

--- application/sky/controller/common.php
+++ application/sky/controller/common.php
@@ -6,10 +6,10 @@ class Login
     public function index()
     {
     	$this->assign('js_file','../view/login/js/index.html');
-        $this->check_login();;
+        $this->check_login();
     }
     public function check_login()
     {
-    	return 111;
+    	return 222;
     }
 }
